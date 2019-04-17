bddd94db07452af2efac7dcee41a57fb384e7052
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2019-04-17 22:15:18 +0700

    Updates

---
+++ precache-manifest.3977e19838f5fc404c2bed342c8e7354.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a82ccc67f1508d318d87",
+    "url": "/funbox-map/static/js/runtime~main.45228355.js"
+  },
+  {
+    "revision": "0e87348c413a2d0fd39e",
+    "url": "/funbox-map/static/js/main.db0becd5.chunk.js"
+  },
+  {
+    "revision": "5b3954863535f500cfcd",
+    "url": "/funbox-map/static/js/2.3efd20ea.chunk.js"
+  },
+  {
+    "revision": "0e87348c413a2d0fd39e",
+    "url": "/funbox-map/static/css/main.d4024471.chunk.css"
+  },
+  {
+    "revision": "52b2060f848435364e55c001a1ae33eb",
+    "url": "/funbox-map/index.html"
+  }
+];
